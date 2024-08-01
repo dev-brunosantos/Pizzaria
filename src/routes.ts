3fecddbbeac3f87f6f92aeb95bbb71f705f8ccd1
dev-brunosantos <brunosflauta@gmail.com>
2024-08-01 17:53:38 -0300

    Enviando pedido

--- src/routes.ts
+++ src/routes.ts
@@ -18,6 +18,7 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemControllers';
+import { SendOrderController } from './controllers/order/SendOrderController';
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -47,6 +48,8 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 
 // CRIANDO PEDIDOS (ORDERS)
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
+// ENVIANDO O PEDIDO ( REMOVE O CAMPOS DE "RASCUNHO" )
+router.put('/order', isAuthenticated, new SendOrderController().handle)
 // FECHANDO AS MESAS ( PROCEDIMENTO REALIZADO QUANDO PEDIDO AINDA NÃO FOI FINALIZADO )
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
@@ -54,5 +57,8 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 // REMOVENDO ITENS DO PEDIDO
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+
+// ENVIANDO PEDIDO
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 export { router }

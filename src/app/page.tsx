f10c46f707e48ea489d7383b657b56c4fad68d9b
dev-brunosantos <brunosflauta@gmail.com>
2024-08-11 12:42:06 -0300

    Salvando cookie para acessar rotas restritas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
 import Link from 'next/link'
 import styles from './page.module.scss'
+import { api } from '../services/api'
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
 
 export default function Page() {
+
+  async function handleLogin(formData: FormData) {
+    "use server"
+
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    if(email === "" || password === "") {
+      return;
+    }
+
+    try {
+      
+      const response = await api.post("/session", {
+        email,
+        password
+      })
+
+      if(!response.data.token) {
+        return;
+      }
+
+      console.log(response.data)
+
+      const expressTime = 60 * 60 * 24 * 30 * 1000
+      cookies().set("session", response.data.token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production"
+      })
+      
+    } catch (error) {
+      console.log(error)
+      return;
+    }
+
+    redirect("/dashboard")
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
@@ -14,7 +57,7 @@ export default function Page() {
         </h1>
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}>
             <input
               type="text"
               required

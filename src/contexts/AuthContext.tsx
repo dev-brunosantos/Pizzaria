6d1d88c0c989b280c0d41e6a5c3ab059427d2330
dev-brunosantos <brunosflauta@gmail.com>
2024-09-06 23:16:19 -0300

    Criando funcionalidade para manter o usuário logado após reiniciar a aplicação

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, ReactNode } from "react";
+import React, { useState, createContext, ReactNode, useEffect } from "react";
 import { api } from "../services/api";
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
@@ -6,6 +6,8 @@ type AuthContextData = {
     user: UserProps;
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) => Promise<void>;
+    loadingAuth: boolean;
+    loading: boolean;
 }
 
 type UserProps = {
@@ -36,8 +38,33 @@ export function AuthProvider({ children }: AhtuProvideProps) {
     })
 
     const [loadingAuth, setLoadingAuth] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     const isAuthenticated = !!user.name; // CONVERÇÃO DE STRING PARA BOOLEAN COM O SINAL !!
+
+    useEffect(() => {
+        async function getUser() {
+            // PEGANDO OS DADOS DO ASYNCSTORAGE
+            const userInfor = await AsyncStorage.getItem('@sugeitopizzaria')
+            let hasUSer: UserProps = JSON.parse(userInfor || '{}')
+
+            // VERIFICAR SE RECEBEMOS AS INFORMAÇÕES DELE
+            if (Object.keys(hasUSer).length > 0) {
+                api.defaults.headers.common['Authorization'] = `Bearer ${hasUSer.token}`
+
+                setUser({
+                    id: hasUSer.id,
+                    name: hasUSer.name,
+                    email: hasUSer.email,
+                    token: hasUSer.token
+                })
+            }
+
+            setLoading(false)
+        }
+
+        getUser()
+    }, [])
 
     async function signIn({ email, password }: SignInProps) {
         setLoadingAuth(true)
@@ -65,7 +92,7 @@ export function AuthProvider({ children }: AhtuProvideProps) {
     }
 
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }} >
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, loading, loadingAuth }} >
             {children}
         </AuthContext.Provider>
     )
